4e5dfdfbe1d40d198d93076b69eaf109e75c4eba
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-04-22 09:09:33 +0100

    integeres

---
+++ number.py
@@ -0,0 +1,40 @@
+#integeres
+
+>>> 3 * 2
+>>> 3 - 2
+>>> 2 + 3
+>>> 2 / 3
+
+>>> 2 ** 3
+>>> 3 ** 3 
+>>> 10 ** 6
+
+>>> 6 + 4*9
+#print (3 + 4) * 7
+
+float
+>>> 0.1 + 0.1
+>>> 0.2 + 0.2
+>>> 2 * 0.1
+>>> 2 * 0.2
+
+>>> 0.2 + 0.1
+>>> 3 * 0.1
+
+>>> 4/2
+
+
+# float 
+
+>>> 0.1 + 0.1
+>>> 0.2 + 0.2
+>>> 2 * 0.1
+>>> 2 * 0.2
+
+#arbitrary answer sometimes, it is not only percular to python, it's all language
+>>> 0.2 + 0.1
+>>> 3 * 0.1
+>>> 
+# multiple assignment
+
+x,y,z = 0,0,0
