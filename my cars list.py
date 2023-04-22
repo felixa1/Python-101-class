f3c763eafda3d5819af9524e536b1460b48f2217
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-04-22 09:04:42 +0100

    Create my cars list.py

---
+++ my cars list.py
@@ -0,0 +1,26 @@
+>>> mylist = ['chervolet','camry','lyncon','mersedise benz','rolsroys','G-wagon','land cruser','prado','parjero']
+>>> mylist= ['chervolet','camry','lyncon','mersedise benz','rolsroys','G-wagon','land cruser','prado','parjero']
+>>> mylist = ['chervolet','camry','lyncon','mersedise benz','rolsroys','G-wagon','land cruser','prado','parjero']
+>>> print(mylist)
+['chervolet', 'camry', 'lyncon', 'mersedise benz', 'rolsroys', 'G-wagon', 'land cruser', 'prado', 'parjero']
+>>> 
+>>> mylist[2] = 'jarguar'
+>>> 
+>>> print(mylist)
+['chervolet', 'camry', 'jarguar', 'mersedise benz', 'rolsroys', 'G-wagon', 'land cruser', 'prado', 'parjero']
+>>> 
+>>> mylist[4] = 'innoson'
+>>> 
+>>> print(mylist)
+['chervolet', 'camry', 'jarguar', 'mersedise benz', 'innoson', 'G-wagon', 'land cruser', 'prado', 'parjero']
+>>> print(mylist)
+['chervolet', 'camry', 'jarguar', 'mersedise benz', 'innoson', 'G-wagon', 'land cruser', 'prado', 'parjero']
+>>> 
+>>> mylist.append('rollroys')
+>>> mylist.append('dulos')
+>>> mylist.append('isuzu')
+>>> mylist.append('elementor')
+>>> mylist.append('seanar')
+>>> print(mylist)
+['chervolet', 'carmry', 'jarguar', 'mersedise benz', 'innoson', 'G-wagon', 'land cruser', 'prado', 'parjero', 'rollroys', 'dulos', 'isuzu', 'elementor', 'seanar']
+>>> 
