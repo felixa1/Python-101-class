e4d6e2d8c58fb21ad362057fe74b16c0372da1af
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-04-22 08:58:18 +0100

    Create Full_name.py
    hands on exercise

---
+++ Full_name.py
@@ -0,0 +1,96 @@
+first_name = "jide"
+last_name = "lawson"
+full_name = f"{first_name} {last_name}" 
+#print(full_name)
+
+first_name = "jide"
+middle_name = "kester"
+last_name = "lawson"
+full_name = f"{first_name} {middle_name} {last_name}" 
+#print(full_name)
+
+
+first_name = "Etiosa="
+second_name ="lekki phase1,maruwa,elf,ikate,elegushi,chisco"
+last_name = "lawson"
+full_name = f"{first_name} {second_name} {last_name}" 
+#print(full_name)
+
+firststreet_name = "Etiosa communities="
+secondstreet_name ="lekki phase1 street,maruwa crycent,elf avenue,ikate crycent,elegushi avenue,chisco busstop"
+laststreet_name = "lawson avenue"
+fullstreet_name = f"{firststreet_name} {secondstreet_name} {laststreet_name}" 
+#print(fullstreet_name)
+
+local_gvlag = "LAGOS STATE LOCAL GOVERNMENTS: ajeromi lg, koshofe lg, ifelodun lg, araromi lg, ikeja lg, amuwo odofin lg, apapa lg, oshodi isolo lg, maroko lg, agege lg"
+#print(local_gvlag)
+
+Names_oflocalgovt = "LAGOS STATE LG COLLECTED PVC'S:"
+#print(Names_oflocalgovt)
+Names_oflocalgovt =  "1 koshofe lg 6354772"
+#print(Names_oflocalgovt)
+Names_oflocalgovt =  "2 ajaromi lg 4537263"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "3 ifelodun lg 654663"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "4 araromi lg 435278"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "5 ikeja lg 76352"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "6 ladipo lg 987693"
+#print(Names_oflocalgovt)
+Names_oflocalgovt =  "7 maroko lg 36524"
+#print(Names_oflocalgovt)
+Names_oflocalgovt =  "8 badore lg 26543"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "9 langbasa lg 1234"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "10 ebutemeta lg 876"
+#print(Names_oflocalgovt)
+Names_oflocalgovt = "11 yaba lg  567"
+#print(Names_oflocalgovt)
+
+#print(f"hello, {full_name.title()}!")
+message = f"hello, {full_name.title()}!"
+#print(message)
+
+header= ('fastway to write the list syntax')
+#print(header)
+print("languages:\n1.python\n2.c\n3.javascript")
+print("\n\t1.ajeromi LG\n2.koshofe LG\n\t3.ifelodun LG\n4.araromi LG\n\t5.ikeja LG\n6.amuwo odofin LG\n\t7.apapa LG\n8.oshodi isolo LG\n\t9.maroko LG\n10.agege LG")
+
+>>> Segs_friends = ['jide', 'tayo','lara', 'tolani','florence','gerarld','kelvin','chris','tom','jephtar','lucus','thompson']
+>>> Segs_friends.append('kolapo')
+>>> Segs_friends.append('dolapo')
+>>> Segs_friends.insert(3,'dolapo')
+>>> Segs_friends.insert(3,'jolade')
+>>> Segs_friends.insert(8,'funsho'upper())
+  File "<stdin>", line 1
+    Segs_friends.insert(8,'funsho'upper())
+                          ^^^^^^^^^^^^^^^
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> Segs_friends.insert(8,'funsho'.upper())
+>>> Segs_friends.insert(5,'john'.upper())
+>>> Segs_friends.insert(7,'titlope'.title())
+>>> 
+>>> print(Segs_friends)
+['jide', 'tayo', 'lara', 'jolade', 'dolapo', 'JOHN', 'tolani', 'Titlope', 'florence', 'gerarld', 'FUNSHO', 'kelvin', 'chris', 'tom', 'jephtar', 'lucus', 'thompson', 'kolapo', 'dolapo']
+>>> max(Segs_friends)
+'tom'
+>>> min(Segs_friends)
+'FUNSHO'
+>>> sum(Segs_friends)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+>>> print(Segs_friends
+... 
+... 
+... 
+  File "<stdin>", line 1
+    print(Segs_friends
+         ^
+SyntaxError: '(' was never closed
+>>> print(Segs_friends)
+['jide', 'tayo', 'lara', 'jolade', 'dolapo', 'JOHN', 'tolani', 'Titlope', 'florence', 'gerarld', 'FUNSHO', 'kelvin', 'chris', 'tom', 'jephtar', 'lucus', 'thompson', 'kolapo', 'dolapo']
