d76e4c197d49cd495d0d4c03bd5a466ad964f595
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-04-22 09:05:36 +0100

    Create name.py

---
+++ name.py
@@ -0,0 +1,19 @@
+name ="ada wande"
+#print(name. title())
+
+#def Etiosa =("lekki phase1,maruwa,elf,ikate,elegushi,chisco")
+#print('Etiosa'()) 
+
+name = "Jide lawson"
+#print(name.title())
+
+name = "Ade Lolade"
+print(name.lower())
+print(name.upper())
+
+#print("Python")
+#print("\tPython")
+#print("languages:\npython\nc\njavascript")
+#print("languages:\n1.python\n2.c\n3.javascript")
+#print("\t1.ajeromi lg\t2.koshofe lg\t3.ifelodun lg\t4.araromi lg,") 
+#print("\n\t1.ajeromi lg\n\t2.koshofe lg\n\t3.ifelodun lg\n\t4.araromi lg,") 
