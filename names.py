4f4907d5fae5a39e4ab3336c29483b894d36879f
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-04-22 09:08:03 +0100

    Create names.py

---
+++ names.py
@@ -0,0 +1,20 @@
+#print("Hello Jane and Titi") 
+#print("Hello Jane Thomas, would you like to travel to germany with me tommorrow?")
+
+name_c = "jane omolola"
+#print(name_c.upper())
+#print(name_c.lower())
+#print('1.Bishop TD Jakes said and i quote,"that money runs from blindness, end of quote!')
+#print('2.Benjamin Franklyn saids and i quote, He who loves lifes ,"should not squander time! end of quote')
+
+#print ('2.Benjamin Franklyn saids and i quote, He who loves lifes ,"should not squander time! end of quote')
+#print("Segun \n it is only a man with right keys that smiles behind a close door!")
+#print('message')
+title = {f'jane omolola.title()!'}
+#print(title)         
+title = {'jane omolola.title'}
+#print(title) 
+#print(" Franklyn  ")
+print('Franklyn lstrip()')
+print('Franklyn rstrip()')
+print('Franklyn strip()')
